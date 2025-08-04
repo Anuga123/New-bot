f4b1eb2c6e076e7a88047b59e1b5dde3d67546f6
Anuga Senithu De Silva <anjanee.uragaha@gmail.com>
2025-08-04 20:53:01 +0530

    Update gen-id.js

--- gen-id.js
+++ gen-id.js
@@ -1,6 +1,6 @@
 function makeid(num = 4) {
   let result = "";
-  let characters = "ANUWH-MD-ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let characters = "ANUWH-MD_BCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   var characters9 = characters.length;
   for (var i = 0; i < num; i++) {
     result += characters.charAt(Math.floor(Math.random() * characters9));
